5d1b1019daf0c45f87ef46570c7416fc61a34952
SteffYang <wonderwallfrank@gmail.com>
2015-05-31 22:09:47 +0800

    basic files

---
+++ index.php
@@ -0,0 +1,15 @@
+<<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<link rel="stylesheet" type="text/css" href="/css/common.css">
+	<link rel="stylesheet" type="text/css" href="/css/jquery.fullPage.css">
+	<script type="text/javascript" src="js/jquery-1.9.1.min.js"></script>
+	<script type="text/javascript" src="js/jquery.fullPage.min.js"></script>
+	<script type="text/javascript" src="js/jssor.utils.js"></script>
+	<script type="text/javascript" src="js/jssor.slider.js"></script>
+</head>
+<body>
+
+</body>
+</html>
