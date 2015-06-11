c125efcbaf494940b7c46cfc912c4c92fe08a83e
yaohappy <yaohappy216@gmail.com>
2015-06-11 11:03:22 +0800

    update

---
+++ logout.php
@@ -0,0 +1,14 @@
+<?php
+	ob_start();
+	session_start() ;
+	unset($_SESSION["account"]) ;
+	unset($_SESSION["memberType"]) ;
+?>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+</head>
+<body>
+<?php header ("Location:index.php"); ?>
+</body>
+</html>
---
+++ user_home.php
@@ -0,0 +1,4 @@
+<?php
+	echo "i am here";
+?>
+<a href="logout.php" data-toggle="modal">登出</a>
